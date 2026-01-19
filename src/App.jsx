0b9b89e1bcbbd0d1a1deb1a2349523f96196aeaa
Adelon <midimaddie@gmail.com>
2026-01-19 16:35:00 -0600

    omg

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router basename="/adelon.studio">
+    <Router basename="/">
       <div className="container">
         <Header />
         <Routes>
